a722b7fef2b75292911b19ea5f5569d03ab26c71
Juan Coronel <jccoronel12.88@gmail.com>
2026-07-01 23:24:27 -0300

    feat: agregada persistencia con EF 10.0.9

---
+++ Dsw2026Ej15.Data/Dsw2026Ej15DbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Dsw2026Ej15.Domain;         
+namespace Dsw2026Ej15.Data
+{
+    public class Dsw2026Ej15DbContext : DbContext
+    {
+        public Dsw2026Ej15DbContext(DbContextOptions<Dsw2026Ej15DbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Speciality> Specialities { get; set; }
+    }
+}
+
---
+++ Dsw2026Ej15.Data/Repositories/PersistenceEf.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Dsw2026Ej15.Domain;
+
+namespace Dsw2026Ej15.Data.Repositories
+{
+    public class PersistenceEf : IPersistence
+    {
+        // Asegúrate de que use el contexto real de Carlos: Dsw2026Ej15DbContext
+        private readonly Dsw2026Ej15DbContext _context;
+
+        // El constructor también debe recibir Dsw2026Ej15DbContext
+        public PersistenceEf(Dsw2026Ej15DbContext context)
+        {
+            _context = context;
+        }
+
+        public void Guardar<T>(T entidad) where T : class
+        {
+            _context.Set<T>().Add(entidad);
+            _context.SaveChanges();
+        }
+
+        public T ObtenerPorId<T>(int id) where T : class
+        {
+            return _context.Set<T>().Find(id);
+        }
+    }
+}
+
+
